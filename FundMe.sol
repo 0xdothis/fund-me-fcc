8b7987dade78df7dc40a95465f99aed9dba9eb36
Sikiru Olagoke <61022383+0xdothis@users.noreply.github.com>
2025-02-18 12:33:59 +0100

    Update FundMe.sol
    I created a function in the PriceConverter.sol library that allows us to convert usd to wei equivalent of eth
    Removed the constant from the variable name  and assigned the function to the MINIMUM_USD variable

--- FundMe.sol
+++ FundMe.sol
@@ -14,14 +14,14 @@ contract FundMe {
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+     uint256 public MINIMUM_USD = PriceConverter.convertUsdToEth(50);
     
     constructor() {
         i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD.getConversionRate(), "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
